cff5393f254a2e3e609e11bcd761024e1275d81d
Maroo Deepak kumar EXT <Deepakkumar.Maroo@iter.org>
2025-07-03 09:38:50 +0200

    Resolved minor bugs

--- frontend/src/components/NewsletterCard.js
+++ frontend/src/components/NewsletterCard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Calendar, ArrowRight } from 'lucide-react';
 import 'react-quill/dist/quill.snow.css';
+import sanitizeHtml from 'sanitize-html';
 
 const NewsletterCard = ({ newsletter }) => {
   const formatDate = (dateString) => {
@@ -26,7 +27,34 @@ const NewsletterCard = ({ newsletter }) => {
         <div
           className="ql-editor mb-4 prose max-w-none line-clamp-3"
           style={{ background: 'transparent', padding: 0, minHeight: 0 }}
-          dangerouslySetInnerHTML={{ __html: newsletter.excerpt || newsletter.content }}
+          dangerouslySetInnerHTML={{ __html: sanitizeHtml(newsletter.excerpt || newsletter.content, {
+            allowedTags: [
+              'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol', 'li', 'b', 'i', 'strong', 'em', 'img', 'br', 'span', 'div', 'hr', 'pre', 'code', 'table', 'thead', 'tbody', 'tr', 'th', 'td'
+            ],
+            allowedAttributes: {
+              a: ['href', 'name', 'target', 'rel', 'class'],
+              img: ['src', 'alt', 'width', 'height', 'style', 'class'],
+              '*': ['style', 'class']
+            },
+            allowedStyles: {
+              '*': {
+                'color': [/^.*$/],
+                'background-color': [/^.*$/],
+                'text-align': [/^.*$/],
+                'font-weight': [/^.*$/],
+                'font-size': [/^.*$/],
+                'font-family': [/^.*$/],
+                'width': [/^.*$/],
+                'height': [/^.*$/],
+                'border': [/^.*$/],
+                'margin': [/^.*$/],
+                'padding': [/^.*$/],
+                'display': [/^.*$/]
+              }
+            },
+            allowedSchemes: ['http', 'https', 'data'],
+            allowProtocolRelative: true
+          }) }}
         />
         <Link
           to={`/newsletter/${newsletter.id || newsletter._id}`}
